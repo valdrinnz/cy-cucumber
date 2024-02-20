e30630b9b1db61b60d0315cbd5e031f2b26dc8ec
valdrinnz <valdrinnexus@gmail.com>
2024-02-20 10:52:46 +0100

    add configuration for cucumber

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'cypress'
 
+const {  addCucumberPreprocessorPlugin} = require("@badeball/cypress-cucumber-preprocessor");
+const {  preprocessor } = require("@badeball/cypress-cucumber-preprocessor/browserify");
+
+async function setupNodeEvents(
+  on: Cypress.PluginEvents,
+  config: Cypress.PluginConfigOptions
+): Promise<Cypress.PluginConfigOptions> {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await addCucumberPreprocessorPlugin(on, config);
+
+  on(
+    "file:preprocessor",
+    preprocessor(config, {
+      typescript: require.resolve("typescript"),
+    })
+  );
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
+
 export default defineConfig({
   e2e: {
-    //  
+    specPattern: "**/*.feature",
+    setupNodeEvents,
+ 
   },
 })
