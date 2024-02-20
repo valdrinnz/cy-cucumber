8f431aeeb600a03302172002c65e600fbbdc0df8
valdrinnz <valdrinnexus@gmail.com>
2024-02-20 23:08:49 +0100

    add comments for to do page

--- cypress/e2e/steps/pages/ToDo/ToDoPage.ts
+++ cypress/e2e/steps/pages/ToDo/ToDoPage.ts
@@ -5,6 +5,7 @@ export default class ToDoPage {
     private readonly INPUT = '[data-test="new-todo"]'
     private readonly CHECKBOX = 'input[type=checkbox]'
 
+    // Visits the home page of the todo application and asserts the URL path
     openHomePage(): void {
         cy.visit('/todo')
         cy.location().should((loc) => {
@@ -12,42 +13,55 @@ export default class ToDoPage {
         })
     }
 
+    // Verifies the presence and length of todo items in the list
     verifyToDoHeading(): void {
         cy.get(this.TO_DO_HEADING_LOCATOR).should('be.visible').and('have.length', 2)
     }
 
+    // Verifies the presence of a specific todo item in the list
+    // @param index - Index of the todo item to verify
+    // @param text - Text to match for the todo item
     verifyItemsInList(index: number, text: string): void {
         cy.get(this.TO_DO_HEADING_LOCATOR).eq(index).should('have.text', text)
     }
 
+    // Asserts the count of remaining todo items
+    // @param text - Text to match for the remaining items count
     toDoCount(text: string) : void {
         cy.get(this.TO_DO_COUNT).should('contain', text)
     }
 
+    // Finds and clicks on the checkbox for the last todo item
     whatNeedsToBeDoneInput() : void {
         cy.get(this.INPUT).should('be.visible')
     }
 
+    // Type a  new todo item into the input field
     typeTextInInput(text : string): void {
         cy.get(this.INPUT).type(text)
     }
 
+    // Finds Element by label and assert it by visibility
     findElementByLabelText(text: string) : void {
         cy.contains(text).should('be.visible')
     }
 
+    // Find the parent element  of an element 
     findClosestElement(element: string) : void {
         cy.get(element).parent()
     }
 
+    // Finds view button
     getViewButton(text: string) : void {
         cy.contains(text).parent()
     }
 
+    // Finds checkbox
     findCheckBox() {
         cy.get(this.CHECKBOX)
     }
 
+    // Clicks in last checkbox
     clickCheckbox() {
         cy.get(this.CHECKBOX).last().click({force:true})
     }
