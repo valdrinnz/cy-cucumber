e6d2cb846a86b620a61d71bd1af6b581b41692f2
valdrinnz <valdrinnexus@gmail.com>
2024-02-20 17:48:29 +0100

    add toDoPage

---
+++ cypress/e2e/steps/pages/ToDo/ToDoPage.ts
@@ -0,0 +1,30 @@
+export default class ToDoPage {
+
+    private readonly TO_DO_HEADING_LOCATOR = '.todo-list li'
+    private readonly TO_DO_COUNT = '.todo-count'
+    private readonly INPUT = 'input[placeholder="What needs to be done?"]'
+
+    openHomePage(): void {
+        cy.visit('/todo')
+        cy.location().should((loc) => {
+            expect(loc.pathname).to.contain('todo')
+        })
+        
+    }
+
+    verifyToDoHeading(): void {
+        cy.get(this.TO_DO_HEADING_LOCATOR).should('be.visible').and('have.length', 2)
+    }
+
+    verifyItemsInList(index: number, text: string): void {
+        cy.get(this.TO_DO_HEADING_LOCATOR).eq(index).should('have.text', text)
+    }
+
+    toDoCount(text: string) : void {
+        cy.get(this.TO_DO_COUNT).should('contain', text)
+    }
+
+    whatNeedsToBeDoneInput() : void {
+        cy.get(this.INPUT).should('be.visible')
+    }
+}
