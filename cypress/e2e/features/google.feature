512a092da1f32e42bd44dbf39b7162b16844b125
valdrinnz <valdrinnexus@gmail.com>
2024-02-20 17:47:39 +0100

    remove dummy test cases

--- cypress/e2e/features/google.feature
+++
@@ -1,8 +0,0 @@
-Feature: Google Search
-    As a user,
-    I want to visit Google's homepage
-    So that I can search for information
-
-Scenario: Visit Google
-    Given I open Google homepage
-    Then I should see the search input field
--- cypress/e2e/steps/stepDefinitions/google.ts
+++
@@ -1,8 +0,0 @@
-import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
-Given('I open Google homepage', () => {
-  cy.visit('https://www.google.com');
-});
-
-Then('I should see the search input field', () => {
-  cy.get('input').should('exist');
-});
